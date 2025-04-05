d104c233a85599a968692d0c73f83895154a2933
hexawayy <hexawayy@gmail.com>
2025-04-05 07:39:23 -0400

    add new route

--- Backend/app.js
+++ Backend/app.js
@@ -58,6 +58,10 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get("/hexaway", (req, res)=>{
+  res.send("hello i'm nothing")
+})
+
 // Routes
 app.use("/search", searchRouter);
 app.use("/images", imageRouter);
